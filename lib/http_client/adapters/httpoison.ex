b9caa6aad41a953acfcc24e9dfdbbf900b3c22bf
Andrew Yudin <pasarian@gmail.com>
2021-11-01 14:38:55 +0600

    Implement Proxy settings

--- lib/http_client/adapters/httpoison.ex
+++ lib/http_client/adapters/httpoison.ex
@@ -13,6 +13,8 @@ defmodule HTTPClient.Adapters.HTTPoison do
 
   @behaviour HTTPClient.Adapter
 
+  @delay 1000
+
   @impl true
   def request(method, url, body, headers, options) do
     perform_request(method, url, headers, body, options)
@@ -43,12 +45,23 @@ defmodule HTTPClient.Adapters.HTTPoison do
     perform_request(:delete, url, headers, "", options)
   end
 
-  defp perform_request(method, url, headers, body, options) do
+  defp perform_request(method, url, headers, body, options, attempt \\ 0) do
+    options = setup_proxy(options)
     options = add_basic_auth_option(options, options[:basic_auth])
 
     case HTTPoison.request(method, url, body, headers, options) do
       {:ok, %{status_code: status, body: body, headers: headers, request: request}} ->
         {:ok, %Response{status: status, body: body, headers: headers, request_url: request.url}}
+
+      {:error, %HTTPoison.Error{id: nil, reason: :proxy_error}} ->
+        case attempt < 5 do
+          true ->
+            Process.sleep(attempt * @delay)
+            perform_request(method, url, headers, body, options, attempt + 1)
+
+          false ->
+            {:error, %Error{reason: :proxy_error}}
+        end
 
       {:error, error} ->
         {:error, %Error{reason: error.reason}}
@@ -59,5 +72,20 @@ defmodule HTTPClient.Adapters.HTTPoison do
 
   defp add_basic_auth_option(options, {username, password}) do
     put_in(options, [:hackney], basic_auth: {username, password})
+  end
+
+  defp setup_proxy(options) do
+    case Application.get_env(:http_client, :proxy, nil) do
+      nil -> options
+      proxies -> add_proxy(options, proxies)
+    end
+  end
+
+  defp add_proxy(options, proxy) when is_map(proxy) do
+    Keyword.put(options, :proxy, "#{proxy.scheme}://#{proxy.address}:#{proxy.port}")
+  end
+
+  defp add_proxy(options, proxies) when is_list(proxies) do
+    add_proxy(options, Enum.random(proxies))
   end
 end
